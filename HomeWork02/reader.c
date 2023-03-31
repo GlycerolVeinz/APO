827d1837ba9138010a8c37c9863ecd960eb08647
GlycerolVeinz <safrankou.matvei@gmail.com>
2023-03-31 10:35:08 +0200

    realised that we aren't using binary files))))))

--- HomeWork02/reader.c
+++ HomeWork02/reader.c
@@ -7,7 +7,7 @@
 	#define EXIT_SUCCESS 0
 #endif
 #define ERR_FILE 100
-
+#define ERR_PARAM 101
 void err_ext(int err_code);
 
 
@@ -16,11 +16,18 @@ int main(int argc, char const *argv[])
 {
 	if (argc <= 1) err_ext(FALSE_START);
 
-	FILE *o_file = fopen(argv[1],"bw");
-	if(!o_file) ERR_FILE;
+	FILE *o_file = fopen(argv[1],"r+");
+	if(!o_file) err_ext(ERR_FILE);
+
+	char pp, six;
+	int width, height, max_hue;
+	if (5 != fscanf(o_file, "%c %c %d %d %d", &pp, &six, &width, &height, &max_hue)) err_ext(ERR_PARAM);
+	if ((pp != 'P') || (six != '6') || max_hue != 255) err_ext(ERR_PARAM); 
 
 	
-	
+
+
+	fclose(o_file);
 	return EXIT_SUCCESS;
 }
 // EO_MAIN--------------------------------------------------------------
@@ -37,8 +44,10 @@ void err_ext(int err_code){
 	case ERR_FILE:
 		err_msg = "Error: could not open a file\n";
 		break;
+	default:
+		err_msg = "Error: this error is yet to be named\n";
+		break;
 	}
-
 	fprintf(stderr, "%s", err_msg);
 	exit(err_code);
 }
